9c0f49b3a06315c86580851f9f213b1214a12751
sharkdp <davidpeter@web.de>
2018-03-15 20:18:36 +0100

    Use same regex for second fd command

--- warm-cache-simple-pattern.sh
+++ warm-cache-simple-pattern.sh
@@ -5,7 +5,7 @@ source "prelude.sh"
 COMMAND_FIND="find '$SEARCH_ROOT' -iregex '.*[0-9]\\.jpg$'"
 COMMAND_FIND2="find '$SEARCH_ROOT' -iname '*[0-9].jpg'"
 COMMAND_FD="fd -HI '.*[0-9]\\.jpg$' '$SEARCH_ROOT'"
-COMMAND_FD2="fd '[0-9]\\.jpg$' '$SEARCH_ROOT'"
+COMMAND_FD2="fd '.*[0-9]\\.jpg$' '$SEARCH_ROOT'"
 
 hyperfine --warmup "$WARMUP_COUNT" \
     "$COMMAND_FIND" "$COMMAND_FIND2" "$COMMAND_FD" "$COMMAND_FD2"
