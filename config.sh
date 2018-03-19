09eb1535c7b5e7ddd320a5ca1bbd94a029df4ae1
sharkdp <davidpeter@web.de>
2018-03-19 22:31:22 +0100

    Increase warmup count

--- config.sh
+++ config.sh
@@ -7,7 +7,7 @@ export SEARCH_ROOT="$HOME"
 export CHECK_DIFF="true"
 
 # Number of warmup runs for "warm cache" benchmarks
-export WARMUP_COUNT=3
+export WARMUP_COUNT=5
 
 # Cache-drop command for "cold cache" benchmarks
 export RESET_CACHES="sync; echo 3 | sudo tee /proc/sys/vm/drop_caches"
